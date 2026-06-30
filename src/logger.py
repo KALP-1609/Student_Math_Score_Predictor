4ee972475acad4b9c7c03cfad6b1e4e2a5d2e094
KALP-1609 <kalp.b0450@gmail.com>
2026-06-30 15:17:14 +0530

    Found the best ML model and saved the model in a pickle file

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,18 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-logs_path = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_path, exist_ok=True)
+today = datetime.now().strftime("%Y-%m-%d")
+timestamp = datetime.now().strftime("%H-%M-%S")
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOGS_DIR = os.path.join(PROJECT_ROOT, "logs", today)
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOGS_DIR, f"{timestamp}.log")
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
+    format="[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s",
 )
-
-if __name__ == "__main__":
-    logging.info("Logging has started")
